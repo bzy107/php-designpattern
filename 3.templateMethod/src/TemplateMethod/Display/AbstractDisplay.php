b02110f14ae9501e4fa2115a047d530ce8fd8124
bzy107 <bzy107@gmail.com>
2025-04-01 17:01:15 +0900

    feat: template method pattern

---
+++ 3.templateMethod/src/TemplateMethod/Display/AbstractDisplay.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TemplateMethod\TemplateMethod\Display;
+
+abstract class AbstractDisplay
+{
+    abstract public function opens(): string;
+
+    abstract public function prints(): string;
+
+    abstract public function closes(): string;
+
+    final public function display(): string
+    {
+        $display = $this->opens();
+        for ($i = 0; $i < 5; $i++) {
+            $display .= $this->prints();
+        }
+        $display .= $this->closes();
+        return $display;
+    }
+}
---
+++ 3.templateMethod/src/TemplateMethod/Display/ChapterDisplay.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TemplateMethod\TemplateMethod\Display;
+
+class ChapterDisplay extends AbstractDisplay
+{
+    public function __construct(private string $string)
+    {
+    }
+
+    public function opens(): string
+    {
+        return '++++++';
+    }
+
+    public function prints(): string
+    {
+        return $this->string;
+    }
+
+    public function closes(): string
+    {
+        return '------';
+    }
+}
