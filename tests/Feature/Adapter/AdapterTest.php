e8620ad52209d899fd57e7449799c5b3e5aa8e0c
bzy107 <bzy107@gmail.com>
2025-04-01 15:06:39 +0900

    test: adapter pattern

---
+++ tests/Feature/Adapter/AdapterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Adapter\Adapter\PrintBanner;
+
+it('can be adapted', function () {
+    $print = new PrintBanner('PrintBanner');
+    expect('(PrintBanner)')
+        ->toBeString()
+        ->toBe($print->printWeak());
+
+    expect('*PrintBanner*')
+        ->toBeString()
+        ->toBe($print->printStrong());
+});
